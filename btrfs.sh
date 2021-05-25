4826063e6284bd7b06c05ce5bd67cbb3ec571db0
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-25 17:49:27 +0300

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -5,33 +5,13 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates       || #Blue                                                          #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-sgdisk -Z /dev/sda
-sgdisk -n 0:0:+512M -t 1:ef00 -n 0:0:+0 -t 2:8300 /dev/sda
-mkfs.vfat /dev/sda1 ; mkfs.btrfs -f /dev/sda2
-mount /dev/sda2 /mnt ; cd /mnt
-btrfs subvolume create @ ; btrfs subvolume create @usr ; btrfs subvolume create @var
-btrfs subvolume create @lib ; btrfs subvolume create @opt ; btrfs subvolume create @etc
-btrfs subvolume create @home ; btrfs subvolume create @bin ; btrfs subvolume create @dev
-btrfs subvolume create @srv ; btrfs subvolume create @tmp ; btrfs subvolume create @media
-cd ; umount /mnt
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@ /dev/sda2 /mnt
-mkdir /mnt/{boot,home,usr,var,lib,opt,etc,sbin,bin,dev,srv,tmp,media}
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@home /dev/sda2 /mnt/home
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@usr /dev/sda2 /mnt/usr
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@var /dev/sda2 /mnt/var
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@lib /dev/sda2 /mnt/lib
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@opt /dev/sda2 /mnt/opt
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@etc /dev/sda2 /mnt/etc
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@bin /dev/sda2 /mnt/bin
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@dev /dev/sda2 /mnt/dev
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@srv /dev/sda2 /mnt/srv
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@tmp /dev/sda2 /mnt/tmp
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@media /dev/sda2 /mnt/media
-mount /dev/sda1 /mnt/boot
-# Tips                  || Yeah, this thing down here is gona' take a while                   #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Color Templates       || #Patience                                                          #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-reflector --completion-percent 50 --download-timeout 21 -f 5 --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
-timedatectl set-ntp true ; pacman -Syy >/dev/null 2>&1
-pacman-key –-init ; pacman-key –-populate archlinux ; pacman-key -–refresh-keys >/dev/null 2>&1
+lsblk -do name,size -e 7,11 ; read -r -p "$(tput setaf 6)$(tput bold)Device name: $(tput sgr0)" sda ;
+BT="/dev/${sda}" ;sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$sda ; PB="$(ls /dev/* | grep -E "^${BT}p?1$")" 
+PR="$(ls /dev/* | grep -E "^${BT}p?2$")" ;mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" ; mount "${PR}" /mnt ; cd /mnt ;
+BSC="btrfs subvolume create @" ;MNT="mount -o noatime,compress=zstd,discard=async,subvol=@" PMT="${PR} /mnt"
+$BSC ; {$BSC}usr ; {$BSC}var ; {$BSC}lib ; {$BSC}opt ; {$BSC}etc ;{$BSC}home ; {$BSC}bin ; {$BSC}dev ;{$BSC}srv ;
+{$BSC}tmp ; {$BSC}media ; cd ; umount /mnt ; ${MNT} {PMT} ; mkdir /mnt/{boot,home,usr,var,lib,opt,etc,sbin,bin,dev,srv,tmp,media}
+${MNT}home {PMT} ;${MNT}usr {PMT} ;${MNT}var {PMT} ;${MNT}lib {PMT} ;${MNT}opt {PMT} ; ${MNT}etc {PMT} ;
+${MNT}bin {PMT} ;${MNT}dev {PMT} ;${MNT}srv {PMT} ;${MNT}tmp {PMT}; ${MNT}media {PMT} ;mount "${PB}" /mnt/boot  
+lsblk -o name,size,type,mountpoint -e 7,11 
+#curl -sL https://git.io/Jsde3 --output install.sh ; chmod +x install.sh ;  ./install.sh
