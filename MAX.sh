92588c4886dd767c3bd606868d10c48d464956e9
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-02 16:47:05 +0300

    Update MAX.sh

--- MAX.sh
+++ MAX.sh
@@ -1,26 +1,27 @@
 #!/bin/bash  
-set -euo pipefail 
-setfont ter-120b 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#    Tom Holkenborg a.k.a. Junkie XL.                                                                                                                         ||
-#           13:37 ━━━━❍━ -15:38                                                                                                                                      ||
-#            ↻     ⊲  Ⅱ  ⊳     ↺                                                                                                                                      ||
-#           VOLUME: ▁▂▃▄▅▆▇ 100%                                                                                                                                ||
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-lsblk -do name,size -e 7,11 ; RED='\033[1;31m' ; NOC='\033[0m'; printf "${RED}Device name: ${NOC}" ; read SDA 
-sgdisk -Z /dev/$SDA ; sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$SDA ; BT="/dev/$SDA" ; BC="btrfs su cr @" 
-PB="$(ls /dev/* | grep -E "^${BT}p?1$")" ; PR="$(ls /dev/* | grep -E "^${BT}p?2$")" 
-MU="mount -o noatime,compress=zstd,discard=async,subvol=@" ; mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" 
-mount "${PR}" /mnt ; cd /mnt ; ${BC} ; ${BC}home ; ${BC}snapshots ; ${BC}var_log ; ${BC}usr ; ${BC}lib ; ${BC}opt 
-${BC}etc ; ${BC}bin ; ${BC}dev ; ${BC}srv ; ${BC}tmp ; ${BC}media ; cd ; umount /mnt ; ${MU} "${PR}" /mnt  
-mkdir /mnt/{boot,home,.snapshots,var_log,usr,lib,opt,etc,bin,dev,srv,tmp,media} ; ${MU}home "${PR}" /mnt/home
-${MU}snapshot "${PR}" /mnt/.snapshots ; ${MU}var_log "${PR}" /mnt/var/log ; ${MU}usr "${PR}" /mnt/usr 
-${MU}lib "${PR}" /mnt/lib ; ${MU}lib "${PR}" /mnt/lib ; ${MU}opt "${PR}" /mnt/opt ; ${MU}etc "${PR}" /mnt/etc 
-${MU}bin "${PR}" /mnt/bin ; ${MU}dev "${PR}" /mnt/dev ; ${MU}srv "${PR}" /mnt/srv ; ${MU}tmp "${PR}" /mnt/tmp 
-${MU}media "${PR}" /mnt/media ; mount "${PB}" /mnt/boot ; lsblk -o name,size,type,mountpoint -e 7,11 ; sleep 5
-curl -sL https://git.io/JGWKK > install.sh ; cp install.sh /mnt/install.sh ; chmod +x /mnt/install.sh 
-reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/pacman.d/mirrorlist
-sed -i 's/#Color/Color/' /etc/pacman.conf ; sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
-gpg -k ; pacman-key --init ; pacman-key --populate archlinux >/dev/null
-pacstrap -i /mnt base-devel git ; genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt sh ./install.sh
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+setfont ter-124b 
+A='\e[1;31m' ; B='\e[0m' 
+C='name,size -e 7,11' ; lsblk -do ${C}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Mozart - Moonlight Sonata  ||  A='\e[1;31m' | B='\e[0m' | C="name,size -e 7,11" | read -r D | E="/dev/${D}"
+#  0:35 ━❍──────── -5:32     ||  F='btrfs su cr @' | G='mount -o noatime,compress=zstd,discard=async,subvol=@'
+#       ↻     ⊲  Ⅱ  ⊳     ↺       ||  H='pacman' | I='TotalDownload' | J='MODULES=' | K='/etc/mkinitcpio.conf'
+#    VOLUME: ▁▂▃▄▅▆▇ 100%   ||  L='xf86-video-' | M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)" | N="intel/amd-ucode"
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+printf "${A}Device name: ${B}" ; read -r D ; E="/dev/${D}" ; sgdisk ${E} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p 
+E1="$(ls /dev/* | grep -E "^${E}p?1$")" ; E2="$(ls /dev/* | grep -E "^${E}p?2$")" ; mkfs.vfat "${E1}" ; mkfs.btrfs -fq "${E2}"
+F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@'
+mount "${E2}" /mnt ; cd /mnt ; ${F} ; ${F}home ; ${F}snapshots ; ${F}var_log ; ${F}usr ; ${F}lib ; ${F}opt 
+${F}etc ; ${F}bin ; ${F}dev ; ${F}srv ; ${F}tmp ; ${F}media ; cd ; umount /mnt ; ${G} "${E2}" /mnt 
+mkdir /mnt/{boot,home,.snapshots,var_log,usr,lib,opt,etc,bin,dev,srv,tmp,media} 
+${G}home "${E2}" /mnt/home ; ${G}snapshot "${E2}" /mnt/.snapshots ; ${G}var_log "${E2}" /mnt/var/log ; ${G}usr "${E2}" /mnt/usr 
+${G}lib "${E2}" /mnt/lib ; ${G}lib "${E2}" /mnt/lib ; ${G}opt "${E2}" /mnt/opt ; ${G}etc "${E2}" /mnt/etc 
+${G}bin "${E2}" /mnt/bin ; ${G}dev "${E2}" /mnt/dev ; ${G}srv "${E2}" /mnt/srv ; ${G}tmp "${E2}" /mnt/tmp 
+${G}media "${E2}" /mnt/media ; mount "${E1}" /mnt/boot ; lsblk -o ${C} ; curl -sL https://git.io/JGWKK > /mnt/install.sh
+H='pacman' ; I='TotalDownload' ; sed -i "s/#${I}/${I}/" /etc/${H}.conf ; gpg -k ; ${H}-key --init ; ${H}-key --populate archlinux >/dev/null
+reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/${H}.d/mirrorlist
+J='MODULES=' ; K='/etc/mkinitcpio.conf' ; L='xf86-video-' ; M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)"
+if [[ "${M}" == "Intel" ]] ; then N="intel-ucode ${L}intel" && sed -i "s/${J}()/${J}(i915 btrfs)/" ${K}
+elif [[ "${M}" == "AMD" ]] ; then N="amd-ucode ${L}amdgpu" && sed -i "s/${J}()/${J}(amdgpu btrfs)/" ${K} ; fi
+pacstrap -i /mnt base base-devel linux-lts linux-lts-headers linux-firmware networkmanager efibootmgr grub vim ${N}
+genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt sh ./install.sh
